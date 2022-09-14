5673a3f9629ed495d08bd6dcca30ef18a90e73d6
MiyamomY <y.miyamoto324@gmail.com>
2022-09-14 22:16:20 +0900

    [Add] laravel-database 59

---
+++ laravel-databasee-queries-from-beginner-to-super-advanced/section9/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->count(10)->create()->each(function ($user){
+            $user->posts()->saveMany(Post::factory()->count(mt_rand(2,6))->make());
+        });
+    }
+}
